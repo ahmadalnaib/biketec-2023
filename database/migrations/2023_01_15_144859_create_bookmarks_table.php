d895a332331ff2cebe60bb2fafadd97b02106bab
ahmedhabeeb73 <alnaib888@gmail.com>
2023-01-15 16:30:28 +0100

    add tables

--- database/migrations/2023_01_15_144859_create_bookmarks_table.php
+++ database/migrations/2023_01_15_144859_create_bookmarks_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('bookmarks', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->integer('place_id');
             $table->timestamps();
         });
     }
--- database/migrations/2023_01_15_144930_create_reports_table.php
+++ database/migrations/2023_01_15_144930_create_reports_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('place_url');
             $table->timestamps();
         });
     }
